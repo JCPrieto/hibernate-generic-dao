a600efa53d5610140252af360ebd3470d2069094
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2020-07-04 10:09:35 +0200

    Eliminamos los ejemplos y la documentación original

--- sample/jpa-hibernate-maven/src/main/java/sample/googlecode/genericdao/dao/CitizenDAOImpl.java
+++
@@ -1,7 +0,0 @@
-package sample.googlecode.genericdao.dao;
-
-import sample.googlecode.genericdao.model.Citizen;
-
-public class CitizenDAOImpl extends BaseDAO<Citizen, Long> implements CitizenDAO {
-
-}
